4dfb91c26b55495b91888215684c3c1ec4fe14c4
Gjjansen <gjjansen97@gmail.com>
2016-12-14 12:14:13 +0100

    some mail changes

--- resources/library/mail.php
+++ resources/library/mail.php
@@ -9,18 +9,18 @@
 		$mail->Host = 'mail.jansengames.com';  				  // Specify main and backup SMTP servers
 		$mail->Timeout = 5;					  				  // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;                               // Enable SMTP authentication
-		$mail->Username = 'testmail@jansengames.com';        // SMTP username
+		$mail->Username = 'testmail@jansengames.com';         // SMTP username
 		$mail->Password = 'test123';                          // SMTP password
 		$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 		$mail->Port = 465;                                    // TCP port to connect to
 
 		$mail->setFrom('testmail@jansengames.com', 'Eurodiscount Coevorden');
-		$mail->addAddress('gerritjanjansen@live.nl');     	  // Add a recipient 
+		$mail->addAddress($customerData[0]['email']);     	  // Add a recipient 
 
 		$mail->isHTML(true);                                  // Set email format to HTML
 
 		$mail->Subject = 'Uw reparatie is afgerond';
-		$mail->Body    = '<h2>Beste '.$customerData[0]['voornaam'].' '.$customerData[0]['achternaam'].'</h2>';
+		$mail->Body    = '<h5>Beste '.$customerData[0]['voornaam'].' '.$customerData[0]['achternaam'].'</h5>';
 		$mail->Body    .= '<p>Uw reparatie is afgerond en kan worden opgehaald bij onze winkel.';
 		$mail->Body    .= '<p>Met vriendelijke groet,</p>';
 		$mail->Body    .= '<p>Eurodiscount Coevorden</p>';
