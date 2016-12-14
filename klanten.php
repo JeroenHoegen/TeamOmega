aec3dbfc8561bb3ca21a22874a82e83d014156c4
Gjjansen <gjjansen97@gmail.com>
2016-12-14 15:57:08 +0100

    Fixed a little issue caused by some merging conflict

--- klanten.php
+++ klanten.php
@@ -85,7 +85,7 @@
                     <li><a href="index.php"><i class="fa fa-bullseye"></i> Overzicht</a></li>
                     <li class="active"><a href="klanten.php"><i class="fa fa-tasks"></i> Klanten</a></li>
 					<li><a href="instellingen.php"><i class="fa fa-gear"></i> Instellingen</a></li>
-					<?php if($userData['role'] == getAuthorityLevel('accountsbeheren')) { ?>
+					<?php if($userData['role'] <= getAuthorityLevel('accountsbeheren')) { ?>
 					<li><a href="accounts.php"><i class="fa fa-id-card"></i> Accounts beheren</a></li>
 					<li><a href="rollen-beheren.php"><i class="fa fa-briefcase"></i> Rollen beheren</a></li>
 					<?php } ?>
